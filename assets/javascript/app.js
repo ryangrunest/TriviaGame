87a8353e28a0d9a91020f36b1b47afdd981f5c28
Ryan Grunest <ryangrunest45@gmail.com>
2019-02-18 01:14:30 -0800

    fixed timer bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-
-	// init counter for correctly and incorrectly answered quiz questions
+// init counter for correctly and incorrectly answered quiz questions
 	var correctCount = 0;
 	var incorrectCount = 0;
 	// counter for questions
@@ -11,6 +10,8 @@
 	var clockRunning = false;
 	var time = 10;
 	var ranOutOfTime = 0;
+function main() {
+	
 
 
 	// load first question
@@ -18,7 +19,7 @@
 		// clear jumbotron html
 		$('.jumbotron').html('');
 		// add question and answers divs to jumbotron
-		$('.jumbotron').html('<div>Time Left: <span class="timer">10</span></div><div class="question"></div><div class="answers"></div>');
+		$('.jumbotron').html('<div class="timer">Time Left: 10</div><div class="question"></div><div class="answers"></div>');
 		// load first question into question sub container
 		$('.question').html(questions[questionCount].question);
 		// load possible answers into answers sub container
@@ -29,14 +30,19 @@
 	function count() {
 		// subtract time by 1
 		time--;
-		if (time === 0) {
-			ranOutOfTime++;
-			loadHTML();
-		};
 		// clear span
 		$('.timer').html('');
 		// update UI with current time
-		$('.timer').append(time);
+		$('.timer').append('Time Left: ' + time);
+		if (time === 0) {
+			ranOutOfTime++;
+			clearHTML();
+			stopTimer();
+			$('.timer').html('');
+			// update UI with correct answer
+			$('.question').html('<h3 class="mt-5">The correct answer was: ' + questions[questionCount].correctAnswer + '</h3>');
+			setTimeout(loadHTML, 3000);
+		};
 	};
 	// resets timer to 10 secs
 	function resetTimer() {
@@ -61,32 +67,41 @@
 		// clear html
 		$('.question').html('');$('.answers').html('');
 	};
-	function loadFinalPage() {
-		stopTimer();
-		// load totals of incorrect and correct count
-		$('.jumbotron').html('<h1>Totals:</h1><h3 class="mt-5">Correct: <span class="correct"></span></h3><h3 class="mt-5">Incorrect: <span class="incorrect"></span></h3><h3 class="mt-5">Ran Out Of Time: <span class="ranOutOfTime"></span> Time(s)</h3>');
-		$('.correct').text(correctCount);
-		$('.incorrect').text(incorrectCount);
-		$('.ranOutOfTime').text(ranOutOfTime);
-	};
+	
 
 
 	// load question and answers divs with html based on question count
 	function loadHTML() {
-		resetTimer();
 		// clear divs for questions and answers
 		clearHTML();
+		resetTimer();
+		startTimer();	
+		
 		// add one to count
 		questionCount++;
-		if (questionCount < 4) {
+		if (questionCount < 1) {
 			$('.question').html(questions[questionCount].question);
 			$('.answers').html(questions[questionCount].answers);
-			$('.timer').html('10');
-		} else if (questionCount >=4) {
+			$('.timer').html('Time Left: 10');
+		} else if (questionCount >= 1) {
 			loadFinalPage();
 		};
 	};
+	function loadFinalPage() {
+		stopTimer();
+		resetTimer();
+		// load totals of incorrect and correct count
+		$('.jumbotron').html('<h1>Totals:</h1><h3 class="mt-5">Correct: <span class="correct"></span></h3><h3 class="mt-5">Incorrect: <span class="incorrect"></span></h3><h3 class="mt-5">Ran Out Of Time: <span class="ranOutOfTime"></span> Time(s)</h3><button type="button" onclick="main()" class="btn btn-info mt-5 btn-lg">Try Again?</button>');
+		$('.correct').text(correctCount);
+		$('.incorrect').text(incorrectCount);
+		$('.ranOutOfTime').text(ranOutOfTime);
 
+		correctCount = 0;
+		incorrectCount = 0;
+		ranOutOfTime = 0;
+		questionCount = 0;
+		intervalID = 0;
+	};
 
 
 	// array of questions to load
@@ -97,7 +112,9 @@
 			// anwers html
 			answers: '<h3 id="1">Elementary School</h3><h3 id="2">Middle School</h3><h3 id="3">High School</h3><h3 id="4">College</h3>',
 			// correct answer value
-			value: 3
+			value: 3,
+			// correct answer string
+			correctAnswer: 'High School'
 		}, 
 		{ // question2
 			// questions html
@@ -105,7 +122,9 @@
 			// html
 			answers: '<h3 id="1">Yes!</h3><h3 id="2">No!</h3>',
 			// correct answer value
-			value: 1
+			value: 1,
+			// correct answer string
+			correctAnswer: 'Yes!'
 		},
 		{
 			// questions html
@@ -113,7 +132,9 @@
 			// html
 			answers: '<h3 id="1">Have Children</h3><h3 id="2">Travel To India</h3><h3 id="3">LSD</h3><h3 id="4">Work For An Apple Orchard</h3>',
 			// correct answer value
-			value: 3
+			value: 3,
+			// correct answer string
+			correctAnswer: 'LSD'
 		},
 		{
 			// questions html
@@ -121,7 +142,9 @@
 			// html
 			answers: '<h3 id="1">Portland State University</h3><h3 id="2">Arizona State University</h3><h3 id="3">Reed College</h3><h3 id="4">Harvard</h3>',
 			// correct answer value
-			value: 3
+			value: 3,
+			// correct answer string
+			correctAnswer: 'Reed College'
 		},
 		{
 			// questions html
@@ -129,7 +152,9 @@
 			// html
 			answers: '<h3 id="1">And Thats The Way We Do It</h3><h3 id="2">And One More Thing...</h3><h3 id="3">The Spaceship Has Landed!</h3><h3 id="4">Are You Ready To Rumble?</h3>',
 			// correct answer value
-			value: 2
+			value: 2,
+			// correct answer string
+			correctAnswer: 'And One More Thing...'
 		}
 	];
 
@@ -151,21 +176,28 @@
 			} else { // for h3's (questions)
 				// if correctly guessed
 				if (event.target.id == questions[questionCount].value) {
-					alert('correct');
+					clearHTML();
+					$('.question').html('<h3>Correct!</h3>')
 					// increase correct count
 					correctCount++;
+					stopTimer();
+					$('.timer').html('');
+					setTimeout(loadHTML, 3000);
+
 					// if incorrect guess
 				} else if (event.target.id != questions[questionCount].value) {
-					alert('incorrect');
+					clearHTML();
+					$('.question').html("<h3>Sorry, that's incorrect :(");
 					// increase incorrect count
 					incorrectCount++;
-				}; 
-				loadHTML();			
+					stopTimer();
+					$('.timer').html('');
+					setTimeout(loadHTML, 3000);
+				}; 		
 			};
-		});
+		});	
+};
 
-		if (time === 0) {
-				loadHTML();
-			};
+main();
 
 
